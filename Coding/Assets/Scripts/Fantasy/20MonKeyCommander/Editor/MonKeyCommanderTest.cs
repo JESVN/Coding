95b4df518eeba49138179a1403f4f54776f63dea
LiangWenQiang_cgsl <liangwenqiang@hncgsl.com>
2020-10-19 10:33:22 +0800

    [V1.9.1]1.添加CMD工作流和插件 2.添加Odin插件

---
+++ Coding/Assets/Scripts/Fantasy/20MonKeyCommander/Editor/MonKeyCommanderTest.cs
@@ -0,0 +1,11 @@
+﻿using MonKey;
+using UnityEngine;
+public class MonKeyCommanderTest : MonoBehaviour
+{
+    [Command("ETEditor_RsyncEditor","Rsync同步",Category = "ETEditor")]
+    private static void ShowWindow()
+    {
+        Debug.Log($"{"MonKeyCommanderTest"}");
+    }
+
+}
---
+++ Coding/Assets/Scripts/Fantasy/21UnityIngameDebugConsole/UnityIngameDebugConsoleTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using IngameDebugConsole;
+using UnityEngine;
+public class UnityIngameDebugConsoleTest
+{
+    [ConsoleMethod("TestLog","这是一条测试ToolLoom.RunAsync的Log")]
+    public static void TestAsyncLog()
+    {
+        ToolLoom.RunAsync(() =>
+        {
+            Debug.Log($"{"这是一个测试常规ToolLoom.RunAsync的Log--3秒后会Log加密通话"}");
+            Thread.Sleep(3000);
+            Debug.Log("这是一个测试常规的Log--别比别比，别比巴伯"); 
+        });
+    }
+    [ConsoleMethod("TestAsyncLog","这是一条测试await/async的Log")]
+    public static async void TestAsyncLog2()
+    {
+        Debug.Log($"{"这是一个测试常规的await/async的Log--3秒后会Log加密通话"}");
+        await Task.Delay(3000);
+        Debug.Log("这是一个测试常规的Log--别比别比，别比巴伯"); 
+    }
+}
