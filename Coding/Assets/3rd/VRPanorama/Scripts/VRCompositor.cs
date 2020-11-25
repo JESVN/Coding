db5020360bfd0c0867339a9a4a5cf50cc6f17761
LiangWenQiang_cgsl <1501766557@qq.com>
2020-11-25 14:32:11 +0800

    新增:unity VR Panorama 360 PRO Renderer 3.0用于制作VR全景视频的专业插件

---
+++ Coding/Assets/3rd/VRPanorama/Scripts/VRCompositor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+namespace VRPanorama {
+	[RequireComponent (typeof (VRCapture))]
+public class VRCompositor : MonoBehaviour {
+
+	private RenderTexture thisRT;
+	private Camera camVR;
+	public Camera[] cameraLayers;
+
+
+	void Awake () {
+		camVR = gameObject.GetComponent<Camera>();
+		for(int i = 0; i < cameraLayers.Length; i++){
+			cameraLayers[i].fieldOfView = 100;	
+			cameraLayers[i].renderingPath = RenderingPath.DeferredShading;
+		}
+
+	
+	}
+	
+
+	void Update () {
+
+		thisRT = camVR.targetTexture;
+			for(int i = 0; i < cameraLayers.Length; i++){
+		cameraLayers[i].targetTexture = thisRT;
+		cameraLayers[i].Render();
+		}
+		
+	}
+}
+}
